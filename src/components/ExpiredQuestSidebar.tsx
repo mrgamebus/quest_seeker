8d3a0d15dd86760627e180e7bbd373a2ac2bad5e
MLI Webdev <webdev@maorilandinfo.co.nz>
2026-05-26 09:30:43 +1200

    Add lint fix to ExpiredQuestSidebar

--- src/components/ExpiredQuestSidebar.tsx
+++ src/components/ExpiredQuestSidebar.tsx
@@ -47,7 +47,7 @@ export default function ExpiredQuestSidebar({
           questParticipants={questParticipants}
           tasks={tasks}
           // onPreparePdf={() => onPreparePdf()}
-          isReady={isReady}
+          // isReady={isReady}
         />
       </>
     )
